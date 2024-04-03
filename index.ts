41f36ec539b889fe62c68ac03fad198d95ce80c8
MuhammadKashan-69 <daiumjaved81@gmail.com>
2024-04-04 03:26:22 +0500

    commit
    I commit simple calculator.

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer=await inquirer.prompt([
+    {name:"firstNumber",
+     type: 'number',
+     message:"enter your first number"},
+
+     {name:"secondNumber",
+      type:"number",
+      message:"enter your second number"},
+
+      {name:"Operator",
+      type:"list",
+      message:"select operator to perform task",
+      choices:["addition","subtraction", "multiplication","division"]}
+]);
+
+if(answer.Operator==="addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if (answer.Operator === "subtraction"){
+   console.log(answer.firstNumber - answer.secondNumber);  
+}
+else if (answer.Operator === "multiplication"){
+   console.log(answer.firstNumber *answer.secondNumber);
+}
+else if  (answer.Operator ==='division'){
+   console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Operator");
+}
